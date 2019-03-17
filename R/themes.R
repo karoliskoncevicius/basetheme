603e35b168eab157799d865df87523e79428bac9
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-03-18 01:20:49 +0200

    added void theme

--- R/themes.R
+++ R/themes.R
@@ -6,6 +6,7 @@
 #' 1. \code{basetheme_dark()}\cr
 #' 2. \code{basetheme_default()}\cr
 #' 3. \code{basetheme_minimal()}\cr
+#' 4. \code{basetheme_void()}\cr
 #'
 #' The used parameters are:
 #' 1) A subset of \code{par} parameters that are most universal to all plots.
@@ -162,6 +163,56 @@ basetheme_minimal <- function() {
        xpd          = FALSE,
        yaxs         = "r",
        yaxt         = "s",
+       rect.col     =  NA,
+       rect.density =  numeric(),
+       rect.angle   =  45,
+       rect.lwd     =  1,
+       rect.lty     =  1,
+       rect.border  =  NA,
+       palette      =  palette
+       )
+}
+
+#' @rdname themes
+#' @export
+basetheme_void <- function() {
+  palette <- c("black", "red", "limegreen")
+  list(fg           = "black",
+       adj          = 0.5,
+       ann          = FALSE,
+       bg           = "white",
+       bty          = "n",
+       cex          = 0.8,
+       cex.axis     = 0.8,
+       cex.lab      = 1,
+       cex.main     = 1.2,
+       cex.sub      = 1,
+       col          = "black",
+       col.axis     = "black",
+       col.lab      = "black",
+       col.main     = "black",
+       col.sub      = "black",
+       family       = "",
+       font         = 1,
+       font.axis    = 1,
+       font.lab     = 1,
+       font.main    = 2,
+       font.sub     = 2,
+       lab          = c(5,5,7),
+       las          = 1,
+       lend         = 0,
+       ljoin        = 0,
+       lmitre       = 10,
+       lty          = 1,
+       lwd          = 1,
+       mgp          = c(2,0.5,0),
+       pch          = 19,
+       tck          = -0.01,
+       xaxs         = "i",
+       xaxt         = "n",
+       xpd          = FALSE,
+       yaxs         = "i",
+       yaxt         = "n",
        rect.col     =  NA,
        rect.density =  numeric(),
        rect.angle   =  45,
