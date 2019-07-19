edc890353be6836eda002267b2db2a18e4c99b6e
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-07-19 23:02:02 +0300

    improved num2col

--- R/coltools.R
+++ R/coltools.R
@@ -6,15 +6,23 @@
 #' Main use case is in turning numbers into colors for plots,
 #' especially when different ranges of \code{x} has to be colored differently.
 #'
+#' Color of NA values and values outside of specified interval can be set using
+#' \code{NAcol} argument. Set this to NA to omit the display of such values.
+#'
 #' @param x numeric vector (factors are transformed to numeric)
 #' @param ... colors to build palette from (defaults to jet colors)
 #' @param NAcol color to be used for NA values (defaults to grey)
-#' @param xrange original range of x values (defaults to \code{range(x)})
+#' @param min lowest value of transformed vector (defaults to \code{min(x)})
+#' @param max highest value of transformed vector (defaults to \code{max(x)})
 #'
 #' @return a vector of colors for each element in x.
 #'
 #' @examples
 #'   plot(mtcars$hp, mtcars$mpg, col=num2col(mtcars$mpg), pch=19, cex=2)
+#'
+#'   plot(mtcars$hp, mtcars$mpg, col=num2col(mtcars$mpg, min=20), pch=19, cex=2)
+#'
+#'   plot(mtcars$hp, mtcars$mpg, col=num2col(mtcars$mpg, min=20, NAcol=NA), pch=19, cex=2)
 #'
 #'   cols <- c("lightblue", "cornflowerblue", "orange", "red")
 #'   plot(mtcars$hp, mtcars$mpg, col=num2col(mtcars$mpg, cols), pch=19, cex=2)
@@ -23,24 +31,24 @@
 #'
 #' @author Karolis Koncevičius
 #' @export
-num2col <- function(x, ..., NAcol="#B9BBB6", xrange=range(x, na.rm=TRUE)) {
+num2col <- function(x, ..., NAcol="#B9BBB6",
+                    min=base::min(x, na.rm=TRUE), max=base::max(x, na.rm=TRUE)
+                    ) {
   if(is.factor(x)) x <- as.numeric(x)
-  if(length(xrange) != 2) stop('"xrange" must have 2 elements: min and max')
-  if(min(x, na.rm=TRUE) < xrange[1]) stop('x has values lower than "xrange[1]"')
-  if(max(x, na.rm=TRUE) > xrange[2]) stop('x has values higher than "xrange[2]"')
 
   pal <- c(...)
   if(length(pal) < 1) {
     pal <- c("blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red")
   }
 
-  vals <- x - xrange[1]
-  vals <- vals / (xrange[2]-xrange[1])
+  x[x < min | x > max] <- NA
+  x <- x - min
+  x <- x / (max-min)
 
   ramp <- colorRamp(pal)
-  cols <- vals
+  cols <- x
   cols[] <- NAcol
-  cols[!is.na(vals)] <- rgb(ramp(vals[!is.na(vals)]), maxColorValue=255)
+  cols[!is.na(x)] <- rgb(ramp(x[!is.na(x)]), maxColorValue=255)
   cols
 }
 
